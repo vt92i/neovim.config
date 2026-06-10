638885c1caf5c5822b7b983f17b35c520bf9d621
Fila Morello <benidiktusvio@gmail.com>
2026-06-10 10:32:22 +0700

    set default max size

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -4,8 +4,8 @@ local open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = opts.border or "rounded"
-  -- opts.max_width = opts.max_width or 80
-  -- opts.max_height = opts.max_height or 25
+  opts.max_width = opts.max_width or 80
+  opts.max_height = opts.max_height or 25
 
   return open_floating_preview(contents, syntax, opts, ...)
 end
